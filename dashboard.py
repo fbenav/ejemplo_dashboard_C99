54e28b54b4759ad47f0ba4317896e2d1ae154396
fbenav <francisco.blorca@gmail.com>
2026-04-02 22:11:24 -0300

    [add] Evolutivo de precios

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,6 @@ import plotly.express as px
 import streamlit as st
 
 st.header("Analisis de ventas para vehiculos usados")
-st.write("TO-DO: dashboard en progreso...")
 
 df = pd.read_csv('dataset/vehicles_us.csv')
 
@@ -13,3 +12,13 @@ st.table(df.head())
 st.subheader("Histograma para el kilometraje:")
 fig = px.histogram(df, x="odometer") # crear un histograma
 st.plotly_chart(fig, use_container_width=True)
+
+build_lineplot = st.checkbox('Construir evolución de precio')
+
+if build_lineplot:
+    st.subheader('Evolución de precio promedio segun condicion vehiculo')
+
+    df_2 = df.groupby(['date_posted','condition'])['price'].mean().reset_index()
+    fig_2 = px.line(df_2, x="date_posted", y="price", color = 'condition',
+                title='precio promedio vehiculos según dia de publicación')
+    st.plotly_chart(fig_2, use_container_width=True)
