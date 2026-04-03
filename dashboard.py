591e6f1ed0734dda121a8212810eb2bb55134180
fbenav <francisco.blorca@gmail.com>
2026-04-02 22:07:57 -0300

    [add] Histograma para kilometraje

--- dashboard.py
+++ dashboard.py
@@ -4,3 +4,12 @@ import streamlit as st
 
 st.header("Analisis de ventas para vehiculos usados")
 st.write("TO-DO: dashboard en progreso...")
+
+df = pd.read_csv('dataset/vehicles_us.csv')
+
+st.write("Vista de las 5 primeras filas del dataset:")
+st.table(df.head())
+
+st.subheader("Histograma para el kilometraje:")
+fig = px.histogram(df, x="odometer") # crear un histograma
+st.plotly_chart(fig, use_container_width=True)
